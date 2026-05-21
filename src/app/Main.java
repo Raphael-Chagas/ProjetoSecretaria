9aba5292a03747cbb8fed17ac1b502bf3b3c198b
Raphael-Chagas <daschagas.raphael08@gmail.com>
2026-05-21 05:43:52 -0300

    receba

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,15 @@
 package app;
 
-public class Main {
+import javax.swing.SwingUtilities;
 
+public class Main {
+		
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				TelaAluno tela = new TelaAluno();
+				tela.setVisible(true);
+			}
+		});
+	}
 }
--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -2,4 +2,46 @@ package model;
 
 public class Aluno {
 
+	private int cpfAluno;
+	private String nomeAluno;
+	private String telefoneAluno;
+	private String emailAluno;
+	
+	public Aluno() {}
+	public Aluno(String nomeAluno, String telefoneAluno, String emailAluno) {
+		this.nomeAluno = nomeAluno;
+		this.telefoneAluno = telefoneAluno;
+		this.emailAluno = emailAluno;
+	}
+	public Aluno (int cpfAluno, String nomeAluno, String telefoneAluno, String emailAluno) {
+		this.cpfAluno = cpfAluno;
+		this.nomeAluno = nomeAluno;
+		this.telefoneAluno = telefoneAluno;
+		this.emailAluno = emailAluno;
+	}
+	public int getCpfAluno() {
+		return cpfAluno;
+	}
+	public void setCpfAluno(int cpfAluno) {
+		this.cpfAluno = cpfAluno;
+	}
+	public String getNomeAluno() {
+		return nomeAluno;
+	}
+	public void setNomeAluno(String nomeAluno) {
+		this.nomeAluno = nomeAluno;
+	}
+	public String getTelefoneAluno() {
+		return telefoneAluno;
+	}
+	public void setTelefoneAluno(String telefoneAluno) {
+		this.telefoneAluno = telefoneAluno;
+	}
+	public String getEmailAluno() {
+		return emailAluno;
+	}
+	public void setEmailAluno(String emailAluno) {
+		this.emailAluno = emailAluno;
+	}
+	
 }
