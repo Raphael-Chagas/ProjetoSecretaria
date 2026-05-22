77070ea050569ab115c4299992568a0c5cf5e17f
Raphael-Chagas <daschagas.raphael08@gmail.com>
2026-05-21 23:10:29 -0300

    embaçado esse JAVA

--- src/view/TelaAluno.java
+++ src/view/TelaAluno.java
@@ -41,6 +41,11 @@ public class TelaAluno extends JFrame {
 		private JTextField txtTelefoneResp;
 		private JTextField txtEmailResp;
 		
+	    private JButton btnNovo;
+	    private JButton btnSalvar;
+	    private JButton btnExcluir;
+	    private JButton btnLimpar;
+	    
 		private JTable tabelaAlunos;
 		private DefaultTableModel modeloTabela;
 		
@@ -70,7 +75,183 @@ public class TelaAluno extends JFrame {
 			lblCpfAluno = new JLabel("CPF do Aluno:");
 			txtCpfAluno = new JTextField();
 			
-			lblNomeAluno = new JLabel("Nome do Aluno:")
-		}
+			lblNomeAluno = new JLabel("Nome do Aluno:");
+			txtNomeAluno = new JTextField();
+			
+			lblTelefoneAluno = new JLabel("Telefone de contato:");
+			txtTelefoneAluno = new JTextField();
+			
+			lblEmailAluno = new JLabel("email para contato:");
+			txtEmailAluno = new JTextField();
+			
+			lblCpfResp = new JLabel("CPF do Responsável:");
+			txtCpfResp = new JTextField();
+			
+			lblNomeResp = new JLabel("Nome do Responsável:");
+			txtNomeResp = new JTextField();
+			
+			lblTelefoneResp = new JLabel("Telefone de contato:");
+			txtTelefoneResp = new JTextField();
+			
+			lblEmailResp = new JLabel("email para contato:");
+			txtEmailResp = new JTextField();
 
+			painelFormulario.add(lblRm);
+			painelFormulario.add(txtRm);
+			painelFormulario.add(lblCpfAluno);
+			painelFormulario.add(txtCpfAluno);
+			painelFormulario.add(lblNomeAluno);
+			painelFormulario.add(txtNomeAluno);
+			painelFormulario.add(lblTelefoneAluno);
+			painelFormulario.add(txtTelefoneAluno);
+			painelFormulario.add(lblEmailAluno);
+			painelFormulario.add(txtEmailAluno);
+			
+			painelFormulario.add(lblCpfResp);
+			painelFormulario.add(txtCpfResp);
+			painelFormulario.add(lblNomeResp);
+			painelFormulario.add(txtNomeResp);
+			painelFormulario.add(lblTelefoneResp);
+			painelFormulario.add(txtTelefoneResp);
+			painelFormulario.add(lblEmailResp);
+			painelFormulario.add(txtEmailResp);
+			
+			add(painelFormulario, BorderLayout.NORTH);
+			
+			modeloTabela = new DefaultTableModel(new Object[] { "ID", "Nome", "Email", }, 0) {
+				private static final long serialVersionUID = 10L;
+				
+				public boolean isCellEditable(int row, int column) {
+					return false;
+				}
+			};
+			
+			tabelaAlunos = new JTable(modeloTabela);
+			JScrollPane scrollPane = new JScrollPane(tabelaAlunos);
+			scrollPane.setBorder(BorderFactory.createTitledBorder("Lista de Registros"));
+			add(scrollPane, BorderLayout.CENTER);
+			
+			JPanel painelBotoes = new JPanel(new FlowLayout(FlowLayout.CENTER, 15, 10));
+			
+			btnNovo = new JButton("NOVO");
+			btnSalvar = new JButton("SALVAR");
+			btnExcluir = new JButton("EXCLUIR");
+			btnLimpar = new JButton("LIMPAR");
+			
+			painelBotoes.add(btnNovo);
+			painelBotoes.add(btnSalvar);
+			painelBotoes.add(btnExcluir);
+			painelBotoes.add(btnLimpar);
+			
+			add(painelBotoes, BorderLayout.SOUTH);
+		}	
+		
+	private void configurarEventos() {
+		btnNovo.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					controller.limpar();
+			}
+		});
+		
+		btnSalvar.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+				controller.salvar();
+			}
+		});
+		btnExcluir.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+				controller.excluir();
+			}
+		});
+		btnLimpar.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+				controller.limpar();
+			}
+		});
+		tabelaAlunos.addMouseListener(new java.awt.event.ActionListener() {
+			public void mouseClicked(MouseEvent e) {
+				controller.preencherFormulario();
+			}
+		});
+	}
+
+	public JTextField getTxtRm() {
+		return txtRm;
+	}
+
+	public void setTxtRm(JTextField txtRm) {
+		this.txtRm = txtRm;
+	}
+
+	public JTextField getTxtCpfAluno() {
+		return txtCpfAluno;
+	}
+
+	public void setTxtCpfAluno(JTextField txtCpfAluno) {
+		this.txtCpfAluno = txtCpfAluno;
+	}
+
+	public JTextField getTxtNomeAluno() {
+		return txtNomeAluno;
+	}
+
+	public void setTxtNomeAluno(JTextField txtNomeAluno) {
+		this.txtNomeAluno = txtNomeAluno;
+	}
+
+	public JTextField getTxtTelefoneAluno() {
+		return txtTelefoneAluno;
+	}
+
+	public void setTxtTelefoneAluno(JTextField txtTelefoneAluno) {
+		this.txtTelefoneAluno = txtTelefoneAluno;
+	}
+
+	public JTextField getTxtEmailAluno() {
+		return txtEmailAluno;
+	}
+
+	public void setTxtEmailAluno(JTextField txtEmailAluno) {
+		this.txtEmailAluno = txtEmailAluno;
+	}
+
+	public JTextField getTxtCpfResp() {
+		return txtCpfResp;
+	}
+
+	public void setTxtCpfResp(JTextField txtCpfResp) {
+		this.txtCpfResp = txtCpfResp;
+	}
+
+	public JTextField getTxtNomeResp() {
+		return txtNomeResp;
+	}
+
+	public void setTxtNomeResp(JTextField txtNomeResp) {
+		this.txtNomeResp = txtNomeResp;
+	}
+
+	public JTextField getTxtTelefoneResp() {
+		return txtTelefoneResp;
+	}
+
+	public void setTxtTelefoneResp(JTextField txtTelefoneResp) {
+		this.txtTelefoneResp = txtTelefoneResp;
+	}
+
+	public JTextField getTxtEmailResp() {
+		return txtEmailResp;
+	}
+
+	public void setTxtEmailResp(JTextField txtEmailResp) {
+		this.txtEmailResp = txtEmailResp;
+	}
+
+	public JTable getTabelaAlunos() {
+		return tabelaAlunos;
+	}
+
+	public void setTabelaAlunos(JTable tabelaAlunos) {
+		this.tabelaAlunos = tabelaAlunos;
+	}
 }
